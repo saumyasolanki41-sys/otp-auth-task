98adea75a9a64d1289bc1b237a11ae32c5482827
saumyasolanki41-sys <saumyasolanki41@gmail.com>
2026-09-19 22:13:06 +0530

    Add user model and email verification fields

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const app=require("./app");
 const connectDB=require("./config/db");
-
+const User=require("./models/user");
 const PORT=process.env.PORT ||5000;
 
 const startServer=async()=>{
   try{
     await connectDB();
+
+    await User.init();
+    console.log("User model and indexes are ready");
+    
     app.listen(PORT,()=>{
       console.log(`Server running at http://localhost:${PORT}`);
     });
